72975366511b100821b33d1cf9fab6167dd9391b
yang dong won <didehddnjs89@gmail.com>
2024-04-12 15:07:21 +0900

    back btn 구현 필

--- app/src/main/java/com/example/sparta_personal4_applemarket/main/MainActivity.kt
+++ app/src/main/java/com/example/sparta_personal4_applemarket/main/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.sparta_personal4_applemarket.main
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sparta_personal4_applemarket.ListData
+import com.example.sparta_personal4_applemarket.R
 import com.example.sparta_personal4_applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +34,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvSaleList.adapter = MainRecyclerViewAdapter(ListData.data)
         binding.rvSaleList.layoutManager = LinearLayoutManager(this)
+
+        setSupportActionBar(binding.tbMain)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 
 
@@ -30,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         val builder = AlertDialog.Builder(this)
         builder.setMessage("종료하시겠습니까?")
 
-        val listener = DialogInterface.OnClickListener{ dialog, which ->
+        val listener = DialogInterface.OnClickListener { dialog, which ->
             when (which) {
                 DialogInterface.BUTTON_POSITIVE -> finish()
             }
@@ -40,5 +59,86 @@ class MainActivity : AppCompatActivity() {
         builder.setNegativeButton("아니오", listener)
 
         builder.show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.bell_notification -> {
+                notification()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun notification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+                // 알림 권한이 없다면, 사용자에게 권한 요청
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                    putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                }
+                startActivity(intent)
+            }
+
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            val builder: NotificationCompat.Builder
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channelId = "one-channel"
+                val channelName = "My Channel One"
+                val channel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ).apply {
+                    // 채널에 다양한 정보 설정
+                    description = "apple market notification"
+                    setShowBadge(true)
+                    val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                    val audioAttributes = AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                        .setUsage(AudioAttributes.USAGE_ALARM)
+                        .build()
+                    setSound(uri, audioAttributes)
+                    enableVibration(true)
+                }
+
+                manager.createNotificationChannel(channel)
+                builder = NotificationCompat.Builder(this, channelId)
+            } else builder = NotificationCompat.Builder(this)
+
+            val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_background)
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            builder.run {
+                setSmallIcon(R.mipmap.ic_launcher)
+                setWhen(System.currentTimeMillis())
+                setContentTitle("키워드 알림")
+                setContentText("설정한 키워드 알림입니다..")
+                setStyle(NotificationCompat.BigTextStyle()
+                    .bigText("설정한 키워드 알림입니다. 감사합니다."))
+                setLargeIcon(bitmap)
+//            setStyle(NotificationCompat.BigPictureStyle()
+//                    .bigPicture(bitmap)
+//                    .bigLargeIcon(null))  // hide largeIcon while expanding
+                addAction(R.mipmap.ic_launcher, "Action", pendingIntent)
+            }
+
+
+            manager.notify(11, builder.build())
+        }
     }
 }
